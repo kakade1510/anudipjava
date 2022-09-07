8895af55cffdbd19e454f018a63d9e966557c4ea
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-07 20:44:18 +0530

    Add files via upload

---
+++ NestedWhileLoop.java
@@ -0,0 +1,20 @@
+
+public class NestedWhileLoop {
+
+	public static void main(String[] args) {
+		int i=2;
+		while(i<=5) {
+			System.out.println();
+			int j=1;
+		while(j<=10) {
+			System.out.print(+(i*j));
+			j++;
+		}
+		i++;
+		
+		
+		}
+
+	}
+
+}
