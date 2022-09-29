43293747843d86d651657d80db51284a8b88d17f
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-29 23:55:34 +0530

    Add files via upload
    Project Name :LabTest4
    
    1.A boy has his money deposited Rs 1000,  Rs 1500 and  Rs 2000 in banks-Bank A, Bank B and Bank C respectively. We have to print the money deposited by him in a particular bank.
    Create a abstract class  'Bank' with an abstract method 'getBalance' . Make its three subclasses named 'BankA', 'BankB' and 'BankC' with a method with the same name 'getBalance' which returns the amount deposited in that particular bank. 
    Call the method 'getBalance' by the object of each of the three banks.

---
+++ Bank.java
@@ -0,0 +1,8 @@
+package Bank.com;
+// WAP to show bank deposited money using below methods
+abstract class Bank {
+
+	abstract void getBalance();//created method of getbalance
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,21 @@
+package Bank.com;
+
+public class Main {
+
+	public static void main(String[] args) {
+		{
+			//Create a instance and call get method 
+			Bank A =new BankA();
+			A.getBalance();
+			
+			
+			Bank B =new BankB();
+			B.getBalance();
+			
+			Bank C =new BankC();
+			C.getBalance();
+			
+		}
+
+}
+}
