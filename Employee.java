5e2be704847fa884741230e70dbeb9309853a0c4
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-29 23:58:33 +0530

    Add files via upload
    Project Name :LabTest4
    3.Create a class named 'Member' having the following members:
    Data members
    1 - Name
    2 - Age
    3 - Phone number
    4 - Address
    5 - Salary
    It also has a method named 'printSalary' which prints the salary of the members.
    Two classes 'Employee' and 'Manager' inherits the 'Member' class. The 'Employee' and 'Manager' classes 
    have data members 'specialization' and 'department' respectively. 
    Now, assign name, age, phone number, address and salary to an employee and a manager 
    by making an object of both of these classes and print the same.

---
+++ Employee.java
@@ -0,0 +1,22 @@
+package Member.com;
+
+public class Employee extends Member
+{
+	String Specialization;
+	
+	
+	public String getSpecialization() {
+		return Specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		Specialization = specialization;
+	}
+
+	public Employee() {
+		super();
+		
+	}
+
+	
+}
