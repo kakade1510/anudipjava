609fc198f308426713bec5ffee92a95c5edfd21d
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-29 15:12:10 +0530

    Add files via upload
    Project Lab Test 4
    Q.3.  Create a class named 'Member' having the following members:
    Data members
    1 - Name
    2 - Age
    3 - Phone number
    4 - Address
    5 - Salary
    It also has a method named 'printSalary' which prints the salary of the members.
    Two classes 'Employee' and 'Manager' inherits the 'Member' class. The 'Employee' and 'Manager' classes 
    have data members 'specialization' and 'department' respectively. 
    Now, assign name, age, phone number, address and salary to an employee and a manager 
    by making an object of both of these classes and print the same.

---
+++ Member.java
@@ -0,0 +1,19 @@
+package Member.com;
+
+public class Member {
+
+	String name;
+	int age;
+	String number;
+	String address;
+	int salary;
+	
+	
+	public void printSalary() {
+		System.out.println(salary);
+		
+	}
+	
+
+
+}
