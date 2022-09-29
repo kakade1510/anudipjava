232d1e204e7ee35869eec26d624e31a437868191
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-30 00:48:04 +0530

    Add files via upload
    Project Name :LabTest4
    
    1.A boy has his money deposited Rs 1000,  Rs 1500 and  Rs 2000 in banks-Bank A, Bank B and Bank C respectively. We have to print the money deposited by him in a particular bank.
    Create a abstract class  'Bank' with an abstract method 'getBalance' . Make its three subclasses named 'BankA', 'BankB' and 'BankC' with a method with the same name 'getBalance' which returns the amount deposited in that particular bank. 
    Call the method 'getBalance' by the object of each of the three banks.

--- Main.java
+++ Main.java
@@ -1,45 +1,21 @@
-package Member.com;
+package Bank.com;
 
 public class Main {
 
 	public static void main(String[] args) {
-      Employee emp = new Employee();
-      emp.name= "pratibha";
-      emp.age=46;
-      emp.number="9423878729";
-      emp.address="Honaji nager,Aurangabad-431008";
-      emp.salary=2000;
-      
-      
-      
-      
-    System.out.println("Name of employee is "+emp.name);
-  	System.out.println("age of employee is "+emp.age);
-  	System.out.println("number of employee is "+emp.number);
-  	System.out.println("address of employee is "+emp.address);
-  	System.out.println("salary of employee is "+emp.salary);
-  	
-  	
-  	
-  		
-  		Manager mn = new Manager();  
-  		mn.name= "pratik";
-  		mn.age=28;
-  		mn.number="1234567890";
-  	    mn.address="radha swami colony,Aurangabad-431001";
-  		mn.salary=100000;
-  		
+		{
+			//Create a instance and call get method 
+			Bank A =new BankA();
+			A.getBalance();
+			
+			
+			Bank B =new BankB();
+			B.getBalance();
+			
+			Bank C =new BankC();
+			C.getBalance();
+			
+		}
 
-        System.out.println("\n Name of Manager is " +mn.name);
-    	System.out.println("age of Manager is "+mn.age);
-    	System.out.println("number of Manager is "+mn.number);
-    	System.out.println("address of Manager is "+mn.address);
-    	System.out.println("salary of Managere is "+mn.salary);		
-  		
-      
-      
-      
-      
-	}
-
+}
 }
