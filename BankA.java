dabd1d275719da103e11ffb304222146dcbe09b1
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-29 23:22:10 +0530

    Update BankA.java

--- BankA.java
+++ BankA.java
@@ -2,9 +2,9 @@ package Bank.com;
 
 public class BankA extends Bank{
 
-	void getBalance()
+	void getBalance()//printing getbalance method 
 	{
-		System.out.println("Money deposited in Bank A is Rs 1000");
+		System.out.println("Money deposited in Bank A is Rs 1000");return 0;//returning values
 		
 	}
 
