561e0df5ed71701502885a115ccaab03e9954433
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-09-29 15:10:12 +0530

    Add files via upload
     Project Lab test 4
    Q.2 We have to calculate the area of a rectangle, a square and a circle. Create an interface 'Shape' with three abstract methods 
    namely 'RectangleArea' taking two parameters, 'SquareArea' and 'CircleArea' taking one parameter each. 
    The parameters of 'RectangleArea' are its length and breadth, that of 'SquareArea' is its side and 
    that of 'CircleArea' is its radius. Now create another class 'Area' containing all the three methods 
    'RectangleArea', 'SquareArea' and 'CircleArea' for printing the area of rectangle, square and circle 
    respectively. Create an object of class 'Area' and call all the three methods.

---
+++ BankA.java
@@ -0,0 +1,11 @@
+package Bank.com;
+
+public class BankA extends Bank{
+
+	void getBalance()
+	{
+		System.out.println("Money deposited in Bank A is Rs 1000");
+		
+	}
+
+}
---
+++ BankC.java
@@ -0,0 +1,9 @@
+package Bank.com;
+
+public class BankC extends Bank{
+
+	void getBalance()
+	{
+		System.out.println("Money deposited in Bank C is Rs 2000");
+	}
+}
