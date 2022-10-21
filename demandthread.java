7f57ccad383ee813c789941a163d3a7a5709f834
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-10-21 16:54:35 +0530

    Add files via upload
    project Lab Test 7 
    
    3.Apply InterThread Communication on demand and supply model.
      Create a class Producer with one data member int availableProduct and 
      two methods --demand() and supply().
      Create two threads . In the run() of first thread call demand(int n) and 
      In the run() of second thread call supply(int n).If deamnding number is gretaer than availableProduct
      consumer should wait till supply increases the availableProduct.

---
+++ demandthread.java
@@ -0,0 +1,17 @@
+package MutiThread.com;
+
+public class demandthread extends Thread{
+
+	private Producer pro;
+
+	public demandthread(Producer pro) {
+		super();
+		this.pro = pro;
+
+	}
+	
+	public void run()
+	{
+		pro.demand(1200);
+	}
+}
