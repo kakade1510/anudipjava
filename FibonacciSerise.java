8ba0cd93f5420c180924f7c6a5c55c4efd7190ac
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-10-21 16:29:35 +0530

    Add files via upload
    Project Lab Test 7
    
    Q.2 Write a JAVA program which will generate two threads:(Use Synchronization)

---
+++ FibonacciSerise.java
@@ -0,0 +1,35 @@
+package MutiThread.com;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FibonacciSerise extends Thread {
+	
+    public void run()
+    {
+         try
+         {
+              int a=0, b=1, c=0;
+              BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+              System.out.print("Enter the Limit for fabonacci: ");
+
+              int n = Integer.parseInt(br.readLine());
+              System.out.println("Fibonacci series:");
+              while (n>0)
+              {
+                   System.out.print(c+" ");
+                   a=b;
+                   b=c;
+                   c=a+b;
+                   n=n-1;
+              }
+         }
+         catch (Exception ex)
+         {
+              ex.printStackTrace();
+              try{  
+    		      Thread.sleep(400);  
+    		     }catch(Exception e){System.out.println(e);}  
+   	   }     
+    }
+}
