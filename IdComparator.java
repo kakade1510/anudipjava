028e39fc172f7478111fc33b9c2eb1150e87346d
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-11-03 15:58:23 +0530

    Add files via upload
    Project Lab Test Name 9:
    (StudentList.java)
    1.WAP to create 2 ArrayLists list1 and list2 and add 3 objects of Student in each list,sort the list
     on sid and name  and print(using foreach loop or iterator) elements of both ArrayLists one on one line.

---
+++ IdComparator.java
@@ -0,0 +1,18 @@
+package Collection.com;
+
+import java.util.Comparator;
+
+public class IdComparator implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		
+		if(o1.getId()>o2.getId())
+			return 1;
+		else if(o1.getId()<o2.getId())
+			 return -1;
+		else
+			return 0;
+	}
+
+}
