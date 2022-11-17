b8d1d03835e18fecd0a8693f8893d4e5b15354ac
kakade1510 <113005663+kakade1510@users.noreply.github.com>
2022-11-18 00:15:56 +0530

    Add files via upload
    Project name : LabTestOnJunit
    1.Write a program for addition() and multiplication() of two numbers.
    Write test cases for each of these methods.

---
+++ Add_Multi.java
@@ -0,0 +1,18 @@
+package LabTestOnJunit11;
+
+public class Add_Multi {
+	
+	
+	public int add(int a, int b) //method to addition
+	{
+		return a+b; 
+	} 
+	
+	public int multi(int a, int b) //method to multiply 
+	{
+		return a*b; 
+	}
+
+
+
+}
